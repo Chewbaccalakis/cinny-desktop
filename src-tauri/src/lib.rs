a20d531df4aeef39f955bfd28db24ce9485319f3
Chewbaccalakis <nick@trochalakis.com>
2024-11-27 13:42:29 -0800

    idk, commenting all those lines worked...

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,10 +3,10 @@
     windows_subsystem = "windows"
 )]
 
-#[cfg(target_os = "macos")]
-mod menu;
+//#[cfg(target_os = "macos")]
+//mod menu;
 
-use tauri::{utils::config::AppUrl, WebviewUrl};
+use tauri::{utils::config::FrontendDist, WebviewUrl};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -16,8 +16,8 @@ pub fn run() {
     let url = format!("http://localhost:{}", port).parse().unwrap();
     let window_url = WebviewUrl::External(url);
     // rewrite the config so the IPC is enabled on this URL
-    context.config_mut().build.frontend_dist = AppUrl::Url(window_url.clone());
-    context.config_mut().build.dev_url = AppUrl::Url(window_url.clone());
+//    context.config_mut().build.frontend_dist = FrontendDist::Url(window_url.clone());
+//    context.config_mut().build.dev_url = FrontendDist::Url(window_url.clone());
     let builder = tauri::Builder::default()
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_shell::init())
@@ -30,8 +30,8 @@ pub fn run() {
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_fs::init());
 
-    #[cfg(target_os = "macos")]
-    let builder = builder.menu(menu::menu());
+//    #[cfg(target_os = "macos")]
+//    let builder = builder.menu(menu::menu());
 
     builder
         .plugin(tauri_plugin_localhost::Builder::new(port).build())
