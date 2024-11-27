11b153066a73779c67dfaa12738a4b66502a533d
Chewbaccalakis <nick@trochalakis.com>
2024-11-27 13:57:49 -0800

    again makes it work

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -23,9 +23,9 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_updater::Builder::new().build())
+ //       .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+//        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_fs::init());
